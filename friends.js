cbd538ec87dfb9b77a7cc9af7360baf86ae2242c
Amber Shipley <ashipley2@me.com>
2022-11-10 16:00:10 -0800

    Add code for first problem

---
+++ friends.js
@@ -0,0 +1,9 @@
+var numbers = [10, 20, 30, 40, 50, 60, 70, 80]
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 30) {
+        console.log(numbers[i])
+    }
+}
+
+//Define a variable that stores an Array containing 8 Numbers between 1 and 100.
+//Write a program that checks each Number, and prints out only those above 30.
